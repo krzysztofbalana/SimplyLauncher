4020be7ae22c1d360590225a565a8ada3f501783
Krzysztof Balana <krzysztof.balana@gmail.com>
2017-03-27 23:11:27 +0200

    Refactoring/Cleaning

--- presentation/src/main/java/pl/mobly/simplylauncher/HomeActivity.kt
+++ presentation/src/main/java/pl/mobly/simplylauncher/HomeActivity.kt
@@ -5,6 +5,11 @@ import android.app.Application
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.Consumer
+import org.reactivestreams.Subscription
+import pl.mobly.domain.DomainApplicationInfo
+import pl.mobly.domain.interactors.SystemInfoInteractorImpl
 
 import javax.inject.Inject
 
@@ -12,9 +17,18 @@ import pl.mobly.simplylauncher.common.AppBase
 
 class HomeActivity : Activity() {
 
+    lateinit var disposable:Disposable
+
+    @Inject
+    lateinit var systemInfoInteractorImpl:SystemInfoInteractorImpl
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         AppBase.appComponent.inject(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
     }
 }
