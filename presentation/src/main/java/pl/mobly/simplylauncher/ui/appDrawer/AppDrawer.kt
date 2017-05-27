13f1d9bfbe051fbd1ad892c336bfb26459f4211f
Krzysztof Balana <krzysztof.balana@gmail.com>
2017-05-27 19:06:37 +0200

    further works on handling switcher on AppDrawer view

--- presentation/src/main/java/pl/mobly/simplylauncher/ui/appDrawer/AppDrawer.kt
+++ presentation/src/main/java/pl/mobly/simplylauncher/ui/appDrawer/AppDrawer.kt
@@ -4,9 +4,15 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import android.widget.Toast
+import io.reactivex.disposables.Disposable
+import org.reactivestreams.Subscription
 import pl.mobly.simplylauncher.R
+import pl.mobly.simplylauncher.common.views.SimpleSwitcher
 
 class AppDrawer : LinearLayout {
+	lateinit var disposable:Disposable
+	lateinit var subscription:Subscription
 
 	constructor(context: Context) : super(context) {
 		init()
@@ -27,7 +33,18 @@ class AppDrawer : LinearLayout {
 		val layoutInflater = LayoutInflater.from(context)
 		val view = layoutInflater.inflate(R.layout.app_drawer, this, true) as LinearLayout
 		view.orientation = LinearLayout.VERTICAL
+
+		val view1 = view.findViewById(R.id.switcher) as SimpleSwitcher
+
+		disposable = view1.listenToClicks()
+				.doOnNext { i -> makeToast(i) }
+				.doOnComplete {  }
+				.subscribe()
 	}
 
+
+	private fun makeToast(t: Int?) {
+		Toast.makeText(context, t.toString(), Toast.LENGTH_SHORT).show()
+	}
 
 }
