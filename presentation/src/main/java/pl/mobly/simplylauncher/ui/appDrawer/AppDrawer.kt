f19417b7f054619231a3b3efe138fa6d88bc7488
Krzysztof Balana <krzysztof.balana@gmail.com>
2017-06-06 20:51:14 +0200

    Created View extensions functions kit

--- presentation/src/main/java/pl/mobly/simplylauncher/ui/appDrawer/AppDrawer.kt
+++ presentation/src/main/java/pl/mobly/simplylauncher/ui/appDrawer/AppDrawer.kt
@@ -3,87 +3,77 @@ package pl.mobly.simplylauncher.ui.appDrawer
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import android.widget.LinearLayout
 import io.reactivex.disposables.Disposable
 import pl.mobly.simplylauncher.R
 import pl.mobly.simplylauncher.common.AppBase
 import pl.mobly.simplylauncher.common.views.SimpleSwitcher
+import pl.mobly.simplylauncher.common.makeGone
+import pl.mobly.simplylauncher.common.makeVisible
 import pl.mobly.simplylauncher.ui.appDrawer.grid.AppsGridView
 import pl.mobly.simplylauncher.ui.appDrawer.list.AppsListView
 import javax.inject.Inject
 
 class AppDrawer : LinearLayout, AppDrawerView {
-	@Inject lateinit var presenter: AppDrawerPresenterImpl
+    @Inject lateinit var presenter: AppDrawerPresenterImpl
 
-	lateinit var disposable:Disposable
-	lateinit var parentView:LinearLayout
-	lateinit var simpleSwitcher:SimpleSwitcher
-	lateinit var listView:AppsListView
-	lateinit var gridView:AppsGridView
+    lateinit var disposable: Disposable
+    lateinit var parentView: LinearLayout
+    lateinit var simpleSwitcher: SimpleSwitcher
+    lateinit var listView: AppsListView
+    lateinit var gridView: AppsGridView
 
-	constructor(context: Context) : super(context) {
-		init()
-	}
+    constructor(context: Context) : super(context) {
+        init()
+    }
 
-	constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-		init()
-	}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
 
-	constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
-		init()
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
 
-	}
+    }
 
-	private fun init() {
-		(context.applicationContext as AppBase).createHomeComponent().inject(this)
+    private fun init() {
+        (context.applicationContext as AppBase).createHomeComponent().inject(this)
 
-		val layoutInflater = LayoutInflater.from(context)
-		parentView = layoutInflater.inflate(R.layout.app_drawer, this, true) as LinearLayout
-		parentView.orientation = LinearLayout.VERTICAL
+        val layoutInflater = LayoutInflater.from(context)
+        parentView = layoutInflater.inflate(R.layout.app_drawer, this, true) as LinearLayout
+        parentView.orientation = LinearLayout.VERTICAL
 
-		simpleSwitcher = parentView.findViewById(R.id.switcher) as SimpleSwitcher
-		listView = parentView.findViewById(R.id.view_apps_list) as AppsListView
-		gridView = parentView.findViewById(R.id.view_apps_grid) as AppsGridView
+        simpleSwitcher = parentView.findViewById(R.id.switcher) as SimpleSwitcher
+        listView = parentView.findViewById(R.id.view_apps_list) as AppsListView
+        gridView = parentView.findViewById(R.id.view_apps_grid) as AppsGridView
 
-		disposable = simpleSwitcher.listenToClicks()
-				.doOnNext { i -> notifyHost(i) }
-				.doOnComplete {  }
-				.subscribe()
+        disposable = simpleSwitcher.listenToClicks()
+                .doOnNext { i -> notifyHost(i) }
+                .doOnComplete { }
+                .subscribe()
 
-		presenter.bind(this)
+        presenter.bind(this)
 
-	}
+    }
 
-	private fun notifyHost(i: Int?) {
-		presenter.onSwitcherTapped(i)
-	}
+    private fun notifyHost(i: Int?) {
+        presenter.onSwitcherTapped(i)
+    }
 
-	override fun onDetachedFromWindow() {
-		super.onDetachedFromWindow()
-		presenter.unbind()
-		(context.applicationContext as AppBase).releaseHomeComponent()
-	}
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        presenter.unbind()
+        (context.applicationContext as AppBase).releaseHomeComponent()
+    }
 
-	override fun showList() {
-		makeVisible(listView)
-		makeGone(gridView)
-	}
+    override fun showList() {
+        listView.makeVisible()
+        gridView.makeGone()
+    }
 
-	override fun showGrid() {
-		makeVisible(gridView)
-		makeGone(listView)
-	}
+    override fun showGrid() {
+        gridView.makeVisible()
+        listView.makeGone()
+    }
 
-	fun makeVisible(view:View) {
-		if (view.visibility == View.GONE || view.visibility == View.INVISIBLE) {
-			view.visibility = View.VISIBLE
-		}
-	}
-
-	fun makeGone(view:View) {
-		if (view.visibility == View.VISIBLE) {
-			view.visibility = View.GONE
-		}
-	}
 }
