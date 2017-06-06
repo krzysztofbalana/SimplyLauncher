7819ffef35e1cd5bc55d94a6974d402b3878a75f
Krzysztof Balana <krzysztof.balana@gmail.com>
2017-06-06 20:51:14 +0200

    Created View extensions functions kit

---
+++ presentation/src/main/java/pl/mobly/simplylauncher/common/ViewExtensions.kt
@@ -0,0 +1,32 @@
+package pl.mobly.simplylauncher.common
+
+import android.view.View
+import android.view.ViewGroup
+
+
+fun View.makeVisible() {
+    val visible: Int = View.VISIBLE
+
+    when (this.visibility) {
+        View.INVISIBLE -> this.visibility = visible
+        View.GONE -> this.visibility = visible
+    }
+
+}
+
+fun View.makeGone() {
+    val gone: Int = View.GONE
+
+    when (this.visibility) {
+        View.VISIBLE -> this.visibility = gone
+    }
+}
+
+fun ViewGroup.childList(): List<View> {
+    var list = ArrayList<View>()
+    for (i in 0..this.childCount - 1) {
+        val childAt = this.getChildAt(i) as View
+        list.add(childAt)
+    }
+    return list
+}
